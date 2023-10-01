4779f5a91a26b82ab8e450be23c7d07efcbed926
Riskiabdilahp <124411414+Riskiabdilahp@users.noreply.github.com>
2023-10-01 11:51:54 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,9 @@
+var firstName = "RISKI ABDILAH";
+var lastName = "PRATAMA";
+var age = 24; 
+var isMarried = 27;
+
+console.log (firstName);
+console.log (lastName);
+console.log (age);
+console.log (isMarried);
